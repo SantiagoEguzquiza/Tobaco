e11859942579fb40c4bc75af0dfa78ea39e0484c
LautaroDeLeon <lautaroodeleonn@gmail.com>
2025-04-03 17:08:40 -0300

    act

--- TobacoBackend/TobacoBackend/obj/Debug/net8.0/TobacoBackend.AssemblyInfo.cs
+++ TobacoBackend/TobacoBackend/obj/Debug/net8.0/TobacoBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4450a86e2be581c865d529edb6a8e3fe3ecaac49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34f4040842eeddada8aa163021d80d416e57768")]
 [assembly: System.Reflection.AssemblyProductAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
